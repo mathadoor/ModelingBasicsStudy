95a0e2a8c0d6786b8250e28bf18f4fa12bf50bf2
mathadoor <matharooh2@gmail.com>
2023-05-09 17:17:30 -0400

    Working code. Final Push.

--- CoinToss/main.py
+++ CoinToss/main.py
@@ -1,21 +1,89 @@
-# Here, we perform experiments to determine the probability of getting a head
-# when tossing a coin. We use the maximum likelihood estimator to estimate the
-# probability of getting a head. We use two different estimators: the Bernoulli
-# estimator and the Markov chain estimator. The Bernoulli estimator is the
-# simplest estimator. It assumes that the probability of getting a head is the
-# same for all tosses. The Markov chain estimator assumes that the probability
-# of getting a head depends on the previous toss. If the previous toss was a
-# head, the probability of getting a head is higher. If the previous toss was a
-# tail, the probability of getting a head is lower.
+# Here, we perform experiments to determine the maximum likelihood estimate of the probability of getting a head
+# when tossing a coin.
 
-import numpy
+import numpy as np
+import seaborn as sns
 import matplotlib.pyplot as plt
 import models
 from data_gen import bernoilli_coin, markov_coin
 
-P = 0.6
-DELTA = 0.1
-N = 100
+P = 0.7  # Probability of getting a head
+DELTA = 0.1  # Difference in probability of getting a head when the coin is facing heads up and tails up
+NUM_FLIPS_RANGE = np.arange(10, 100, 10)  # Number of coin tosses per experiment
+NUM_TRIALS = 100  # Number of experiments
+FIG_SIZE = (5, 5)  # Size of the figure
 
-bernoulli_data = bernoilli_coin(P).tosses(N)
-markov_data = markov_coin(P, DELTA).tosses(N)
+
+bernoulli_p_bd = np.zeros((len(NUM_FLIPS_RANGE), NUM_TRIALS))
+markov_p_bd = np.zeros((len(NUM_FLIPS_RANGE), NUM_TRIALS))
+markov_delta_bd = np.zeros((len(NUM_FLIPS_RANGE), NUM_TRIALS))
+bernoulli_p_md = np.zeros((len(NUM_FLIPS_RANGE), NUM_TRIALS))
+markov_p_md = np.zeros((len(NUM_FLIPS_RANGE), NUM_TRIALS))
+markov_delta_md = np.zeros((len(NUM_FLIPS_RANGE), NUM_TRIALS))
+
+# Performing the experiments
+for j in range(len(NUM_FLIPS_RANGE)):
+    M = NUM_FLIPS_RANGE[j]
+
+    for i in range(NUM_TRIALS):
+        # Generating the data
+        bernoulli_data = bernoilli_coin(P).tosses(M)
+        markov_data = markov_coin(P, DELTA).tosses(M)
+
+        # Fitting the bernoulli model and the markov model to the Bernoulli data
+        bernoulli_p_bd[j, i] = models.bernoulli_mle(bernoulli_data)
+        t0, t1 = models.markov_chain_mle(bernoulli_data)
+        markov_p_bd[j, i], markov_delta_bd[j, i] = t0, t1
+
+
+        # Fitting the bernoulli model and the markov model to the Markov data
+        bernoulli_p_md[j, i] = models.bernoulli_mle(markov_data)
+        t0, t1 = models.markov_chain_mle(markov_data)
+        markov_p_md[j, i], markov_delta_md[j, i] = t0, t1
+
+
+# Plotting the results with mean with shaded standard deviation area
+fig, ax = plt.subplots(figsize=FIG_SIZE)
+ax.plot(NUM_FLIPS_RANGE, bernoulli_p_bd.mean(axis=1), alpha=0.5, color='blue', label='Bernoulli', linewidth = 1.0)
+ax.fill_between(NUM_FLIPS_RANGE,
+                bernoulli_p_bd.mean(axis=1) - bernoulli_p_bd.std(axis=1),
+                bernoulli_p_bd.mean(axis=1) + bernoulli_p_bd.std(axis=1),
+                color='#0000bb',
+                alpha=0.1)
+ax.plot(NUM_FLIPS_RANGE, markov_p_bd.mean(axis=1), alpha=0.5, color='red', label='Markov', linewidth = 1.0)
+ax.fill_between(NUM_FLIPS_RANGE,
+                markov_p_bd.mean(axis=1) - markov_p_bd.std(axis=1),
+                markov_p_bd.mean(axis=1) + markov_p_bd.std(axis=1),
+                color='#bb0000',
+                alpha=0.1)
+ax.axhline(y=P, color='black', linestyle='--', label='True value')
+ax.set_xlabel('Number of tosses')
+ax.set_ylabel('Probability of getting a head')
+ax.set_title('Bernoulli data')
+ax.set_xlim([NUM_FLIPS_RANGE[0], NUM_FLIPS_RANGE[-1]])
+ax.set_ylim([0, 1])
+ax.legend(loc='best')
+plt.savefig('plots/bernoulli_data.png')
+
+fig, ax = plt.subplots(figsize=FIG_SIZE)
+ax.plot(NUM_FLIPS_RANGE, bernoulli_p_md.mean(axis=1), alpha=0.5, color='blue', label='Bernoulli', linewidth = 1.0)
+ax.fill_between(NUM_FLIPS_RANGE,
+                bernoulli_p_md.mean(axis=1) - bernoulli_p_md.std(axis=1),
+                bernoulli_p_md.mean(axis=1) + bernoulli_p_md.std(axis=1),
+                color='#0000bb',
+                alpha=0.1)
+ax.plot(NUM_FLIPS_RANGE, markov_p_md.mean(axis=1), alpha=0.5, color='red', label='Markov', linewidth = 1.0)
+ax.fill_between(NUM_FLIPS_RANGE,
+                markov_p_md.mean(axis=1) - markov_p_md.std(axis=1),
+                markov_p_md.mean(axis=1) + markov_p_md.std(axis=1),
+                color='#bb0000',
+                alpha=0.1)
+ax.axhline(y=P, color='black', linestyle='--', label='True value')
+ax.set_xlabel('Number of tosses')
+ax.set_ylabel('Probability of getting a head')
+ax.set_title('Markov data')
+ax.set_xlim([NUM_FLIPS_RANGE[0], NUM_FLIPS_RANGE[-1]])
+ax.set_ylim([0, 1])
+ax.legend(loc='best')
+plt.savefig('plots/markov_data.png')
+
--- CoinToss/models.py
+++ CoinToss/models.py
@@ -1,7 +1,9 @@
-import numpy
+import numpy as np
+
 
 def bernoulli_mle(data):
-    return numpy.mean(data)
+    return np.mean(data)
+
 
 def markov_chain_mle(data):
     '''
@@ -9,9 +11,37 @@ def markov_chain_mle(data):
     the second element given the first element. If the sequence is 11, the probability of the second element being (p + delta).
     If the sequence is 01, the probability of the second element being (p - delta). If the sequence is 10, the probability of
     the second element being (1 - p - delta). If the sequence is 00, the probability of the second element being (1 - p + delta).
+    Count the number of such subsequences as m, n, p, q respectively. The overall probability will be
+    (p + delta)^m * (p - delta)^p * (1 - (p + delta))^n (1 - (p - delta))^q
     :param data:
     :return:
     '''
     # TODO: Implement this
+    m, n, p, q = 0, 0, 0, 0
+    prob = 1
+    for i in range(len(data) - 1):
+        if data[i] == 1 and data[i + 1] == 1:
+            m += 1
+        elif data[i] == 0 and data[i + 1] == 1:
+            n += 1
+        elif data[i] == 1 and data[i + 1] == 0:
+            p += 1
+        else:
+            q += 1
 
-    return numpy.mean(data)
+    # Perform Grid Search on the values of pr and delta for which the log probability is maximum
+    max_prob = -np.inf
+    max_p = 0
+    max_delta = 0
+    for pr in np.linspace(0.01, 0.99, 100):
+        p_s = np.log(pr) if data[0] == 1 else np.log(1 - pr)
+        for delta in np.linspace(0, 0.99, 100):
+            if pr + delta >= 1 or pr - delta <= 0:
+                continue
+            prob = p_s + m * np.log(pr + delta) + p * np.log(pr - delta) + n * np.log(1 - (pr + delta)) + q*np.log(1 - (pr - delta))
+            if prob > max_prob:
+                max_prob = prob
+                max_p = pr
+                max_delta = delta
+
+    return max_p, max_delta
